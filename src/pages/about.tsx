cd81841813acc0dcbcda491c1d566645b4f01bca
Takumi0901 <juventus0901@gmail.com>
2021-04-02 12:58:54 +0900

    test2

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ import { VFC } from 'react'
 import Layout from '../components/Layout'
 
 const AboutPage: VFC = () => (
-  <Layout title="About | Next.js + TypeScript Example">
+  <Layout title="Abouttest | Next.js + TypeScript Example">
     <h1>About</h1>
     <p>This is the about page</p>
     <p>
