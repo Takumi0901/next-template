31d4a7b5c0c6f89f40fdb2e0a6514dc5eb430aae
Takumi0901 <juventus0901@gmail.com>
2021-04-05 09:52:48 +0900

    styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import '../../styles/globals.css'
+import 'src/styles/globals.css'
 
 const App = ({ Component, pageProps }: any) => {
   return (
